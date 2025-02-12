5ff861e1d53018ec270eb37ac4e6366a585d23e4
juyoung <eunh9372@naver.com>
2025-02-12 17:29:15 +0900

    로그인, 로그아웃 기능

--- src/main/java/kr/bit/controller/EventController.java
+++ src/main/java/kr/bit/controller/EventController.java
@@ -1,4 +1,15 @@
 package kr.bit.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/menu/event")
 public class EventController {
+    @GetMapping("/list")
+    public String logList(){
+        return "menu/event/list";
+    }
+
 }
--- src/main/java/kr/bit/controller/LoginController.java
+++ src/main/java/kr/bit/controller/LoginController.java
@@ -1,10 +1,12 @@
 package kr.bit.controller;
 
+import kr.bit.entity.Admins;
+import kr.bit.service.LoginService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpSession;
 
@@ -12,18 +14,41 @@ import javax.servlet.http.HttpSession;
 @RequestMapping("/")
 public class LoginController {
 
+    @Autowired
+    private LoginService loginService;
 
     @GetMapping
     public String root() {
         return "redirect:/auth/login";
     }
 
-
     @GetMapping("/auth/login")
-    public String loginForm() {
+    public String loginForm(Model model) {
+        model.addAttribute("admin", new Admins());
         return "auth/login";
     }
 
 
 
+    @PostMapping("/auth/login")
+    public String auth(@ModelAttribute("admin") Admins admin, BindingResult result) {
+        if (result.hasErrors()) {
+            return "auth/login";
+        }
+
+        Admins loginResult = loginService.adminLogin(admin);
+        if (loginResult != null) {
+            Admins.setLoginStatus(true);
+            return "redirect:/menu/stats";
+        }
+        return "redirect:/auth/login?fail=true";
+    }
+
+    @GetMapping("/logout")
+    public String logout(HttpSession session) {
+
+        session.invalidate();
+        Admins.setLoginStatus(false);
+        return "redirect:/auth/login";
+    }
 }
