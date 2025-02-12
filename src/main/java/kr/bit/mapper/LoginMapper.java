ba31dae0aff4c2d6774b555485fda03b664e3580
juyoung <eunh9372@naver.com>
2025-02-12 17:29:15 +0900

    로그인, 로그아웃 기능

--- src/main/java/kr/bit/mapper/LoginMapper.java
+++ src/main/java/kr/bit/mapper/LoginMapper.java
@@ -1,7 +1,16 @@
 package kr.bit.mapper;
 
+import kr.bit.entity.Admins;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
 
-@Mapper
+
 public interface LoginMapper {
-}
+        @Select("select * " +
+                "from admins " +
+                "where id = #{id} and pass = #{pass}")
+        Admins getLoginAdmin(Admins admins);
+    }
+
+
+
