5a8097e294971d5b3e0086b1531ae1946852c68b
Joe YeonSik <joe6069@naver.com>
2025-02-18 20:02:37 +0900

    거의다

--- src/main/java/kr/bit/controller/AdminController.java
+++ src/main/java/kr/bit/controller/AdminController.java
@@ -2,12 +2,16 @@ package kr.bit.controller;
 
 import kr.bit.entity.Admins;
 import kr.bit.entity.Criteria;
+import kr.bit.entity.PageCre;
 import kr.bit.service.AdminService;
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.parameters.P;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -23,14 +27,48 @@ public class AdminController {
         List<Admins> admins = adminService.getAdmins(criteria);
         model.addAttribute("admins",admins);
 
+        int totalCount = adminService.getTotalCount(criteria);
+
+        PageCre pageCre = new PageCre();
+        pageCre.setCriteria(criteria);
+        pageCre.setTotalCount(totalCount);
+        model.addAttribute("pageCre", pageCre);
+
         return "menu/manager/list";
     }
     @GetMapping("/register")
-    public String managerDetail(){
+    public String managerDetail(Model model){
+        model.addAttribute("admins",new Admins());
         return "menu/manager/register";
     }
+    @GetMapping("/idDuplicate/{id}")
+    public @ResponseBody boolean idDuplicate(@PathVariable("id")String id){
+        return adminService.oneAdmin(id) == null;
+    }
+    @PostMapping("/register_proc")
+    public String registerPrc(@ModelAttribute("admins") Admins admins, RedirectAttributes rttr){
+        boolean result = adminService.registerAdmin(admins);
+        if(result){
+            rttr.addFlashAttribute("msgType","성공");
+            rttr.addFlashAttribute("msg","관리자가 등록 되었습니다.");
+        }else {
+            rttr.addFlashAttribute("msgType","실패");
+            rttr.addFlashAttribute("msg","관리자가 실패 되었습니다.");
+        }
+        return "redirect:/menu/manager/register";
+    }
     @GetMapping("/modify")
-    public String managerBlock(){
+    public String managerBlock(@Param("id") String id, Model model){
+        Admins admin= adminService.oneAdmin(id);
+        model.addAttribute("admin",admin);
         return "menu/manager/modify";
+    }
+    @PutMapping("/modifyProc")
+    public @ResponseBody boolean modifyProc(@RequestBody Admins admin){
+        return adminService.updateAdmin(admin);
+    }
+    @DeleteMapping("/deleteProc/{id}")
+    public @ResponseBody boolean deleteProc(@PathVariable("id")String id){
+        return adminService.deleteAdmin(id);
     }
 }
--- src/main/java/kr/bit/mapper/BoardMapper.java
+++ src/main/java/kr/bit/mapper/BoardMapper.java
@@ -1,7 +1,38 @@
 package kr.bit.mapper;
 
-import org.apache.ibatis.annotations.Mapper;
+import kr.bit.entity.Boards;
+import kr.bit.entity.Criteria;
+import kr.bit.sql.BoardSqlProvider;
+import org.apache.ibatis.annotations.*;
+
+import java.util.List;
 
 @Mapper
 public interface BoardMapper {
+   @SelectProvider(type = BoardSqlProvider.class , method = "getBoards")
+    List<Boards> getBoards(@Param("criteria") Criteria criteria);
+   @SelectProvider(type = BoardSqlProvider.class,method = "getTotalCount")
+    int getTotalCount(@Param("criteria") Criteria criteria);
+   @Select("select * from boards b join users u on b.writer_id = u.user_id where user_id=#{user_id}")
+    Boards getDetailBoard(@Param("user_id") int user_id);
+    @Update("UPDATE boards SET is_blind = CASE WHEN is_blind = true THEN false ELSE true END WHERE writer_id = #{user_id}")
+    int updateBoardBlind(@Param("user_id") int user_id);
+    @Select("SELECT " +
+            "b.id, " +
+            "a.id AS admin_id, " +
+            "b.writer_id, " +
+            "b.admin_writer_id, " +
+            "b.title, " +
+            "b.content, " +
+            "b.created_at, " +
+            "b.heart_count, " +
+            "b.comment_count, " +
+            "b.is_notice, " +
+            "b.is_blind, " +
+            "b.report_count, " +
+            "a.name " +
+            "FROM boards b " +
+            "LEFT JOIN admins a ON b.admin_writer_id = a.admin_id " +
+            "WHERE b.admin_writer_id = #{admin_writer_id}")
+    Boards getDetailNotice(@Param("admin_writer_id") int admin_writer_id);
 }
