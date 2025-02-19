e88bf7458d862ae1d680c5b55d030a065aa71077
juyoung <eunh9372@naver.com>
2025-02-19 22:28:03 +0900

    시큐리티 1차

--- src/main/java/kr/bit/mapper/LoginMapper.java
+++ src/main/java/kr/bit/mapper/LoginMapper.java
@@ -1,18 +1,17 @@
 package kr.bit.mapper;
 
 import kr.bit.entity.Admins;
-import kr.bit.entity.Events;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
-
+@Mapper
 public interface LoginMapper {
+
     @Select("select * " +
             "from admins " +
             "where id = #{id} and pass = #{pass}")
     Admins getLoginAdmin(Admins admins);
+
+    @Select("select * from admins where id = #{id}")
+    Admins findById(String id);
 }
-
-
-
-
